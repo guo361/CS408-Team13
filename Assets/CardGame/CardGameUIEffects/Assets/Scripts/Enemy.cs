42bdb6b2a2f2c497ef5584bb3b54fb1444f404b9
Yuening Xu <xu_830@outlook.com>
2019-02-16 00:16:55 -0500

    add helth bar
    need test

---
+++ Assets/CardGame/CardGameUIEffects/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody2D enemy;
+    public static float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 1;
+        enemy = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
