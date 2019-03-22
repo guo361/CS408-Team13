e771fe550591b71861c9142d1ef128063b3da4d4
Yuening Xu <xu_830@outlook.com>
2019-03-22 19:38:36 -0400

    refine health

--- Assets/script/HealthBarFight.cs
+++ Assets/script/HealthBarFight.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBarFight : MonoBehaviour
 {
     Vector3 localScale;
+
+    public Text Goodp;
     // Start is called before the first frame update
     void Start()
     {
         localScale = transform.localScale;
+        Goodp.text = string.Format("{0} ", Mathf.RoundToInt(Goodegg.healthAmount * 100));
     }
 
     // Update is called once per frame
     void Update()
     {
+        Goodp.text = string.Format("{0} ", Mathf.RoundToInt(Goodegg.healthAmount * 100));
         localScale.x = Goodegg.healthAmount;
         transform.localScale = localScale;
     }
