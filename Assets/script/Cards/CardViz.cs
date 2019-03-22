97d7f7c2885adfb1aacf0ecf81c179698eb5a7ab
Yuening Xu <xu_830@outlook.com>
2019-03-22 00:05:59 -0400

    card scrpit

---
+++ Assets/script/Cards/CardViz.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace SA
+{
+    public class CardViz : MonoBehaviour
+    {
+        public Text title;
+        public Text detail;
+        public Text flavor;
+        public Text artist;
+        public Image art;
+
+        public Card card;
+
+        private void Start()
+        {
+            LoadCard(card);
+        }
+
+        public void LoadCard(Card c)
+        {
+            if(c == null)
+            {
+                return;
+            }
+            card = c;
+            title.text = c.cardName;
+            detail.text = c.cardDetail;
+
+            if (string.IsNullOrEmpty(c.cardFlavor))
+            {
+                flavor.gameObject.SetActive(false);
+            }
+            else {
+                flavor.gameObject.SetActive(true);
+                flavor.text = c.cardFlavor;
+            }
+
+            artist.text = c.artist;
+            art.sprite = c.art;
+        }
+     
+    }
+}
