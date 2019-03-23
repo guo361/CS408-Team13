30a52323ff91420b0ccea4850721de96480c8c0c
Yuening Xu <xu_830@outlook.com>
2019-03-23 01:44:20 -0400

    let each scene match the health
    also add different folder in script to do classification

--- Assets/script/FightScene/Bad2.cs
+++ Assets/script/FightScene/Bad2.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BadEgg : MonoBehaviour
+public class Bad2 : MonoBehaviour
 {
     public static float healthAmount;
     // Start is called before the first frame update
     void Start()
     {
-        healthAmount = PlayerPrefs.GetFloat("EHealth1", 50.0f);
+        healthAmount = PlayerPrefs.GetFloat("EHealth2", 50.0f);
         healthAmount = healthAmount / 100;
-        Debug.Log("enemy" + healthAmount);
+        Debug.Log("enemy2" + healthAmount);
     }
 
     // Update is called once per frame
