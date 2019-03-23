f4730bc8c0aa2d30f8e88d02d62e0f54005c7e39
Yuening Xu <xu_830@outlook.com>
2019-03-22 21:51:01 -0400

    health for all in demo
    initialize health for all enemy.(need different scenes for enemy fight)

---
+++ Assets/script/Enemy3.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy3 : MonoBehaviour
+{
+    public static int Ehealth3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetFloat("EHealth3", 50.0f);
+        Ehealth3 = (int)PlayerPrefs.GetFloat("EHealth3", 50.0f);
+        Debug.Log("enemy 3 health in demo " + PlayerPrefs.GetFloat("EHealth3", 50.0f));
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Ehealth3 == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
