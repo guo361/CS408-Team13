896a913434956f5b0e3484c2c5f70fb7a6a315f0
Hibiki33 <zhong6@purdue.edu>
2019-03-22 21:19:53 -0400

    Add Card prefab and Global Controller

---
+++ Assets/GlobalControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalControl : MonoBehaviour
+{
+    public static GlobalControl Instance;
+    public float HP;
+   
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
