08830799fe713cf5becd3753434911f2bb6a1e37
Yuening Xu <xu_830@outlook.com>
2019-03-23 19:54:58 -0400

    after m

--- Assets/script/FightScene/BadEgg.cs
+++ Assets/script/FightScene/BadEgg.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class BadEgg : MonoBehaviour
 {
     public static float healthAmount;
+    public static int baseATK;
     // Start is called before the first frame update
     void Start()
     {
         healthAmount = PlayerPrefs.GetFloat("EHealth1", 50.0f);
         healthAmount = healthAmount / 100;
+        baseATK = 7;
         Debug.Log("enemy1" + healthAmount);
     }
 
